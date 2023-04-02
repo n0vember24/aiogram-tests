28e157795986ce2e8cb4b1a49f168cd06c99dab0
n0vember24 <minecraft11242006@gmail.com>
2023-04-02 21:48:09 +0500

    test inline queries

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import logging
+from uuid import uuid4
 
 from aiogram import Bot, Dispatcher, executor
-from aiogram.types import Message
+from aiogram.types import Message, InputTextMessageContent, InlineQueryResultArticle, InlineQuery
 
 from config import BOT_TOKEN
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -14,6 +15,14 @@ dp = Dispatcher(bot)
 @dp.message_handler()
 async def start(msg: Message):
     await msg.reply(msg.text)
+
+
+@dp.inline_handler()
+async def inline_echo(inline_query: InlineQuery):
+    text = InputTextMessageContent(inline_query.query or 'Text')
+    result_id = str(uuid4())
+    item = InlineQueryResultArticle(id=result_id, title=f'Результаты запроса: {text}', input_message_content=text)
+    await bot.answer_inline_query(inline_query.id, results=[item], cache_time=1)
 
 
 if __name__ == '__main__':
