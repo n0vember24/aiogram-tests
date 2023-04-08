4ef77bb99225d81d930a53bf089512bf97454f4b
n0vember24 <minecraft11242006@gmail.com>
2023-04-08 15:36:04 +0500

    work with image

--- bot.py
+++ bot.py
@@ -1,156 +1,39 @@
 import logging
-from uuid import uuid4
+import io
 
-import aiogram.utils.markdown as md
+from PIL import Image
 from aiogram import Bot, Dispatcher, executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import Text, RegexpCommandsFilter
-from aiogram.dispatcher.filters.state import StatesGroup, State
-from aiogram.types import Message, InputTextMessageContent, InlineQueryResultArticle, InlineQuery, ChatActions, \
-    ReplyKeyboardRemove, ReplyKeyboardMarkup, ParseMode, KeyboardButton
+from aiogram.types import Message, ContentType, ParseMode
+from rembg import remove
 
 from config import BOT_TOKEN
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 bot = Bot(BOT_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['buttons'])
-async def buttons(msg: Message):
-    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    loc_btn = KeyboardButton('Location', request_location=True)
-    pn_btn = KeyboardButton('Phone Number', request_contact=True)
-    markup.add(loc_btn, pn_btn)
-    await msg.reply('Buttons', reply_markup=markup)
-
-
-@dp.message_handler(commands=['language'])
-async def check_language(msg: Message):
-    locale = msg.from_user.locale
-
-    await msg.reply(md.text(
-        md.bold('Info about your language:'),
-        md.text('🔸', md.bold('Code:'), md.code(locale.language)),
-        md.text('🔸', md.bold('Territory:'), md.code(locale.territory or 'Unknown')),
-        md.text('🔸', md.bold('Language name:'), md.code(locale.language_name)),
-        md.text('🔸', md.bold('English language name:'), md.code(locale.english_name)),
-        sep='\n',
-    ), ParseMode.MARKDOWN)
-
-
-@dp.message_handler(RegexpCommandsFilter(regexp_commands=['start ([0-9]*)']))
-async def send_welcome(msg: Message, regexp_command):
-    await msg.reply(f"You have requested an item with id <code>{regexp_command.group(1)}</code>", ParseMode.HTML)
-
-
-@dp.message_handler(commands='deeplink')
-async def create_deeplink(msg: Message):
-    bot_user = await bot.me
-    bot_username = bot_user.username
-    deeplink = f'https://t.me/{bot_username}?start={msg.from_user.id}'
-    text = (
-        f'Either send a command /item_1234 or follow this link {deeplink} and then click start\n'
-        'It also can be hidden in a inline button\n\n'
-        f'Or just send <code>/start {msg.from_user.id}</code>'
-    )
-    await msg.reply(text, disable_web_page_preview=True)
-
-
-class Form(StatesGroup):
-    name = State()
-    age = State()
-    gender = State()
-
-
-@dp.message_handler(commands='/state')
-async def cmd_start(msg: Message):
-    await Form.name.set()
-    await msg.reply("Добро пожаловать в наш бот!\nДля продолжения введите своё имя:")
-
-
-@dp.message_handler(state='*', commands='cancel')
-@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
-async def cancel_handler(msg: Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-
-    logging.info('Cancelling state %r', current_state)
-    await state.finish()
-    await msg.reply('Действие отменено.', reply_markup=ReplyKeyboardRemove())
-
-
-@dp.message_handler(state=Form.name)
-async def process_name(msg: Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['name'] = msg.text
-
-    await Form.next()
-    await msg.reply("Сколько вам лет?")
-
-
-@dp.message_handler(lambda msg: not msg.text.isdigit(), state=Form.age)
-async def process_age_invalid(msg: Message):
-    return await msg.reply("Возраст пишется цифрами. Пожалуйста введите своё корректный возраст в виде числа.")
-
-
-@dp.message_handler(lambda msg: msg.text.isdigit(), state=Form.age)
-async def process_age(msg: Message, state: FSMContext):
-    # Update state and data
-    await Form.next()
-    await state.update_data(age=int(msg.text))
-
-    # Configure ReplyKeyboardMarkup
-    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    markup.add("Мужик", "Женщина")
-    markup.add("Транс")
-
-    await msg.reply("Какой ваш пол?", reply_markup=markup)
-
-
-@dp.message_handler(lambda msg: msg.text not in ["Мужик", "Женщина", "Транс"], state=Form.gender)
-async def process_gender_invalid(msg: Message):
-    return await msg.reply("Я пока что не знаю такого вида пола... Пожалуйста, введите корректный пол.")
-
-
-@dp.message_handler(state=Form.gender)
-async def process_gender(msg: Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['gender'] = msg.text
-        markup = ReplyKeyboardRemove()
-
-        await bot.send_message(
-            msg.chat.id,
-            md.text(
-                md.text('Я рад вас видеть,', md.bold(data['name'])),
-                md.text('Ваш возраст:', md.code(data['age'])),
-                md.text('Ваш пол:', data['gender']),
-                sep='\n',
-            ),
-            reply_markup=markup,
-            parse_mode=ParseMode.MARKDOWN,
-        )
-
-    await state.finish()
-
-
-@dp.message_handler()
+@dp.message_handler(commands=['start'])
 async def start(msg: Message):
-    # await ChatActions.typing(5)
-    await ChatActions.upload_voice(1)
-    await msg.reply(msg.text)
+    await msg.reply("Я рад видеть вас в нашем боту!")
 
 
-@dp.inline_handler()
-async def inline_echo(inline_query: InlineQuery):
-    text = InputTextMessageContent(inline_query.query or 'Text')
-    result_id = str(uuid4())
-    item = InlineQueryResultArticle(id=result_id, title=f'Результаты запроса: {text}', input_message_content=text)
-    await bot.answer_inline_query(inline_query.id, results=[item], cache_time=1)
+# @dp.message_handler(content_types=ContentType.PHOTO)
+# async def answer_to_photo(msg: Message):
+#     in_img = msg.photo[-1]
+#     down_img = await in_img.download(destination_file=f'input/{in_img.file_unique_id}.jpg')
+#     bytes_img = io.BytesIO(down_img)
+#     rem_img = Image.open(bytes_img)
+#     out_img = remove(rem_img)
+#     out_img.save(f'output/{in_img.file_unique_id}.jpg')
+
+
+@dp.message_handler(content_types=ContentType.PHOTO)
+async def answer_to_photo(msg: Message):
+    inp_photo = msg.photo[-1]
+    caption = f"File Unique ID: `{inp_photo.file_unique_id}`\n*Test Caption*"
+    await msg.reply_photo(inp_photo.file_id, caption, parse_mode=ParseMode.MARKDOWN)
 
 
 if __name__ == '__main__':
